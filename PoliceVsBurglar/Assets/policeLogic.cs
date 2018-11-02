125a6cb9b981751bf3ea159858976c37df3cd41c
tohtoriteppo <teo.haapajarvi@hotmail.com>
2018-11-02 11:49:17 +0200

    Catch the thiefs
    first test

---
+++ PoliceVsBurglar/Assets/policeLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class policeLogic : MonoBehaviour {
+
+    private int playerNum;
+
+	// Use this for initialization
+	void Start () {
+        playerNum = int.Parse(name.Substring(6, 1));
+        GetComponent<movement>().speed = Camera.main.GetComponent<gameController>().policeSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "burglar")
+        {
+            other.gameObject.GetComponent<burglarLogic>().Die();
+        }
+    }
+}
