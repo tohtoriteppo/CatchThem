e5bbfa251369be2d1c6b24dbf7b43e240e8eace8
tohtoriteppo <teo.haapajarvi@hotmail.com>
2018-11-02 12:57:34 +0200

    test

---
+++ PoliceVsBurglar/Assets/Scripts/gameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameController : MonoBehaviour {
+
+    public float slowPerLoot;
+    public float burglarSpeed;
+    public float policeSpeed;
+    public GameObject timeSlider;
+    private float timer = 0;
+    private float timeLimit = 60.0f;
+	// Use this for initialization
+	void Start () {
+        foreach (string t in Input.GetJoystickNames())
+        {
+            Debug.Log(t);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer = Time.timeSinceLevelLoad;
+        timeSlider.GetComponent<Slider>().value = timer / timeLimit;
+		if(timer > timeLimit)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            timer = 0;
+        }
+	}
+
+}
---
+++ PoliceVsBurglar/Assets/Scripts/movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public float speed = 0.1f;
+    private int playerNum;
+	// Use this for initialization
+	void Start () {
+        playerNum = int.Parse(name.Substring(6, 1));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        if (Input.GetButtonDown("p1_button_y"))
+        {
+            Debug.Log("ENS");
+        }
+        transform.position = 
+            new Vector3(transform.position.x + Input.GetAxis("p"+playerNum.ToString()+"_joystick_horizontal") * speed, 
+            transform.position.y,
+            transform.position.z + Input.GetAxis("p" + playerNum.ToString() + "_joystick_vertical") * speed);
+
+    }
+}
