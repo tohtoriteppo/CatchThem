e2a7bd880fcb19a6f599e9aac1f768ff2b47a5e6
Skylerzhang023 <skyler2zhang@gmail.com>
2018-11-06 11:33:48 +0300

    texture

---
+++ PoliceVsBurglar/Assets/Effects/move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+
+}
