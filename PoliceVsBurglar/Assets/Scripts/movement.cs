4ac9758e8f7bca53197149da28b1b5966aab2a56
Skylerzhang023 <skyler2zhang@gmail.com>
2018-11-08 15:20:35 +0300

    prefab for test

--- PoliceVsBurglar/Assets/Scripts/movement.cs
+++ PoliceVsBurglar/Assets/Scripts/movement.cs
@@ -26,8 +26,12 @@ public class movement : MonoBehaviour {
         //For animator
         //idle
         if((Input.GetAxis("p" + playerNum.ToString() + "_joystick_horizontal"))==0 && (Input.GetAxis("p" + playerNum.ToString() + "_joystick_vertical")) == 0){
-                characterAnimator.SetBool("Run", false);
+                characterAnimator.SetBool("StartRun", false);
         }
+        else{
+            characterAnimator.SetBool("StartRun", true);
+        }
+
     }
 
     public void setSpeed(float speedToSet)
